1844572027ea8f1a7486cd76d422cc5bac0f052b
Eliana Choi <eliechoi@terpmail.umd.edu>
2024-05-17 13:41:06 -0400

    Created models.py file, changed admin-dashboard.html route in app.py and created admin.css

--- app.py
+++ app.py
@@ -1,20 +1,9 @@
-from flask import Flask, redirect, render_template, url_for, request, models, create_app, db
-db.create_all(app=create_app())
-
-app = Flask(__name__)
+from flask import Flask, redirect, render_template, url_for, request
+app = Flask(__name__, template_folder='template') 
 
 #Authentication Routes
 @app.route('/')
 def login():
-    return render_template('/login.html')
-
-@app.route('/login', methods=['POST'])
-def login_post():
-    return redirect(url_for('main.profile'))
-
-#Administrator Routes
-@app.route('/admin-dashboard')
-def dashboard ():
     return render_template('/admin-dashboard.html')
 
 #Student Routes
