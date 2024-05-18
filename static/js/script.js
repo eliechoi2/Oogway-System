19dc51b97e2502000b6c0cb36d5e6da2d2fc6471
Eliana Choi <eliechoi@terpmail.umd.edu>
2024-05-18 16:40:23 -0400

    Making inbox rows clickable on admin dashboard

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,12 @@
 function openSubMenu() {
     document.getElementById("floors").style.color = "red";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var rows = document.querySelectorAll(".clickable-row");
+    rows.forEach(function(row) {
+        row.addEventListener("click", function() {
+            window.location.href = row.dataset.href;
+        });
+    });
+});
